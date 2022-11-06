569f69a4220ed7abe98bf7f83786f2046762416a
anand2312 <40204976+anand2312@users.noreply.github.com>
2022-11-06 12:50:06 +0530

    Make session and storage classes

---
+++ src/backend/auth/sessions/session.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+from datetime import datetime, timezone
+from functools import partial
+from uuid import UUID, uuid4
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class Session:
+    """A user session.
+
+    Whenever a user logs in succesfully, a Session object
+    is created and stored in a session storage backend.
+    """
+
+    id: UUID = field(default_factory=uuid4)
+    user_id: UUID
+    # default factory for a timezone-aware datetime
+    created_at: datetime = field(default_factory=partial(datetime.now, timezone.utc))
---
+++ src/backend/auth/sessions/storage.py
@@ -0,0 +1,69 @@
+from abc import ABCMeta, abstractmethod
+from uuid import UUID
+
+from .. import AuthError
+from .session import Session
+
+
+class SessionDoesNotExistError(AuthError):
+    """The specified session did not exist in the storage backend."""
+
+
+class AbstractSessionStorage(metaclass=ABCMeta):
+    """An abstract session storage.
+
+    Classes that implement this ABC will be used to store user session details.
+    """
+
+    @abstractmethod
+    async def get_session(self, id: UUID) -> Session | None:
+        """Gets a session from the storage. Returns None if the session doesn't exist.
+
+        Args:
+            id: The session ID to get
+        Returns:
+            Session: The session data
+            None: The session didn't exist.
+        """
+        ...
+
+    @abstractmethod
+    async def create_session(self, user_id: UUID) -> Session:
+        """Creates a session for the provided user and stores it.
+
+        Args:
+            user_id: The user to create the session for.
+        Returns:
+            The session that was created.
+        """
+        ...
+
+    @abstractmethod
+    async def delete_session(self, id: UUID) -> None:
+        """Deletes a session from the storage.
+
+        Args:
+            id: The session ID to delete
+        Raises:
+            SessionDoesNotExistError: The specified session does not exist.
+        """
+        ...
+
+
+class InMemorySessionStorage(AbstractSessionStorage):  # noqa: D101
+    def __init__(self) -> None:
+        self._sessions = dict[UUID, Session]()
+
+    async def get_session(self, id: UUID) -> Session | None:  # noqa: D102
+        return self._sessions.get(id)
+
+    async def create_session(self, user_id: UUID) -> Session:  # noqa: D102
+        session = Session(user_id=user_id)
+        self._sessions[session.id] = session
+        return session
+
+    async def delete_session(self, id: UUID) -> None:  # noqa: D102
+        try:
+            self._sessions.pop(id)
+        except KeyError as e:
+            raise SessionDoesNotExistError(f"Session {id} does not exist.") from e
