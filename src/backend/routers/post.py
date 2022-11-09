955149709aee9a4b95415c31556583fefd9090d9
anand2312 <40204976+anand2312@users.noreply.github.com>
2022-11-10 03:53:38 +0530

    Make comment endpoints

--- src/backend/routers/post.py
+++ src/backend/routers/post.py
@@ -16,17 +16,23 @@ router = APIRouter(prefix="/post")
 
 @router.get("/")
 async def get_posts(
-    take: int = Query(default=10),
+    take: int = Header(default=10),
     uid: str | None = Query(default=None),
     tag: str | None = Query(default=None),
-    cursor: str | None = Query(default=None),
+    cursor: str | None = Header(default=None),
 ) -> Page[Post]:
-    """Paginate and get posts based on specified filters (user, tag etc)."""
+    """Paginate and get posts based on specified filters (user, tag etc).
+
+    Expects pagination headers:
+        take: The number of items to be retrieved in one page
+        cursor: The ID of the last fetched item
+    """
     filters = {}
     if uid:
         filters["author_id"] = uid
     if tag:
         filters["tag"] = {"tag_name": tag}
+    filters["deleted"] = False
     return await paginate(Post, take, cursor, where=filters, order={"created_at": "desc"})
 
 
@@ -34,7 +40,7 @@ async def get_posts(
 async def get_post(id: str) -> PostDetails:
     """Get full details of a specific post."""
     post = await Post.prisma().find_first(
-        where={"id": id}, include={"tags": True, "media": True, "author": True}
+        where={"id": id, "deleted": False}, include={"tags": True, "media": True, "author": True}
     )
     if not post:
         raise HTTPException(404, "Post not found")
@@ -55,7 +61,7 @@ async def create_post(
     await is_authorized(user_id, session_id, sessions)
     try:
         inserted_post = await Post.prisma().create(
-            {
+            data={
                 "author_id": str(user_id),
                 "title": post.title,
                 "text_content": post.text_content,
@@ -79,6 +85,8 @@ async def delete_post(
     """Delete a post."""
     await is_authorized(user_id, session_id, sessions)
     try:
+        # this endpoint only soft-deletes
+        #
         deleted_post = await Post.prisma().update(data={"deleted": True}, where={"id": id})
     except PrismaError as e:
         logger.warning(f"Could not delete post: {e}")
@@ -86,3 +94,64 @@ async def delete_post(
     if not deleted_post:
         return JSONResponse({"message": "Post did not exist"}, status_code=400)
     return JSONResponse({"message": "Post deleted"}, status_code=200)
+
+
+@router.post("/{post_id}/comments", response_model=PostComment)
+async def create_comment(
+    post_id: str,
+    comment_text: str = Body(),
+    user_id: UUID | None = Header(default=None),
+    session_id: UUID | None = Header(default=None),
+    sessions: AbstractSessionStorage = Depends(get_sessions),
+) -> PostComment:
+    """Create a comment on the specified post."""
+    await is_authorized(user_id, session_id, sessions)
+    try:
+        comment = await PostComment.prisma().create(
+            data={
+                "author_id": str(user_id),
+                "post_id": post_id,
+                "content": comment_text,
+                "author": {"connect": {"id": str(user_id)}},
+                "post": {"connect": {"id": post_id}},
+            }
+        )
+    except PrismaError as e:
+        logger.warning(f"Could not create comment: {e}")
+        raise HTTPException(400, "Could not create the comment due to an internal error")
+    return comment
+
+
+@router.get("/{post_id}/comments")
+async def get_comments(
+    post_id: str, take: int = Header(default=10), cursor: str | None = Header(default=None)
+) -> Page[PostComment]:
+    """Paginate and get the comments of a post.
+
+    Expects pagination headers:
+        take: The number of items to be retrieved in one page
+        cursor: The ID of the last fetched item
+    """
+    return await paginate(
+        PostComment, take, cursor, where={"post_id": post_id}, order={"created_at": "desc"}
+    )
+
+
+@router.delete("/{post_id}/comments/{comment_id}")
+async def delete_comment(
+    post_id: str,
+    comment_id: str,
+    user_id: UUID | None = Header(default=None),
+    session_id: UUID | None = Header(default=None, alias="session-id"),
+    sessions: AbstractSessionStorage = Depends(get_sessions),
+) -> JSONResponse:
+    """Endpoint to delete a comment."""
+    await is_authorized(user_id, session_id, sessions)
+    try:
+        deleted_comment = await PostComment.prisma().delete(where={"id": comment_id})
+    except PrismaError as e:
+        logger.warning(f"Could not delete comment: {e}")
+        raise HTTPException(400, "Could not delete the comment due to an internal error")
+    if not deleted_comment:
+        return JSONResponse({"message": "Comment did not exist"}, status_code=400)
+    return JSONResponse({"message": "Comment deleted"}, status_code=200)
