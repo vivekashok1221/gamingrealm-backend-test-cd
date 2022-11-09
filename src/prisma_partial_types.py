da99343daacc0bbc7f440d9b40f4e52fb5fcf0dd
vivekashok1221 <vivekashok1221@gmail.com>
2022-11-09 17:04:50 +0530

    Add UserProfile model

--- src/prisma_partial_types.py
+++ src/prisma_partial_types.py
@@ -3,5 +3,12 @@
 
 from prisma.models import User
 
-User.create_partial("UserInLogin_", include={"username", "password"})
-User.create_partial("UserProfile", exclude={"password"})
+User.create_partial(
+    "UserInLogin_",
+    include={
+        "username",
+        "password",
+    },
+)
+
+User.create_partial("UserProfile", include={"id", "username", "email", "created_at"})
