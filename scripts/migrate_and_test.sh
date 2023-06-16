9248d26f0d9618afd1f59eac01e3b6bb7ed357ae
anand2312 <40204976+anand2312@users.noreply.github.com>
2023-06-16 20:21:42 +0530

    Use postgres container alone to run tests

--- scripts/migrate_and_test.sh
+++ scripts/migrate_and_test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+docker run --name test-gr-postgres -e POSTGRES_PASSWORD=adminpw -e POSTGRES_USER=admin -e POSTGRES_DB=gamginrealmdb --network=host --rm -d postgres:latest
 poetry run prisma db push
 poetry run pytest
+docker stop test-gr-postgres
