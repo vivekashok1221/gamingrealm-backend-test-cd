90678eab135616f9e8db4f4051f7a66031a90510
vivekashok1221 <vivekashok1221@gmail.com>
2023-06-18 15:24:38 +0530

    Pin Postgres image to v15

--- scripts/migrate_and_test.sh
+++ scripts/migrate_and_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --name test-gr-postgres -e POSTGRES_PASSWORD=adminpw -e POSTGRES_USER=admin -e POSTGRES_DB=gamginrealmdb --network=host --rm -d postgres:latest
+docker run --name test-gr-postgres -e POSTGRES_PASSWORD=adminpw -e POSTGRES_USER=admin -e POSTGRES_DB=gamginrealmdb --network=host --rm -d postgres:15
 poetry run prisma db push
 poetry run pytest
 docker stop test-gr-postgres
