a4e29199fa428acc467f2faf16320152bb623f49
anand2312 <40204976+anand2312@users.noreply.github.com>
2023-07-13 13:23:03 +0530

    Remove prisma generated model for user in login routes

--- prisma/prisma_partial_types.py
+++ prisma/prisma_partial_types.py
@@ -3,12 +3,4 @@
 
 from prisma.models import User
 
-User.create_partial(
-    "UserInLogin_",
-    include={
-        "username",
-        "password",
-    },
-)
-
 User.create_partial("UserProfile", include={"id", "username", "email", "created_at"})
